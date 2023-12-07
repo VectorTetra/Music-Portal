4a34eba60c6d79dd96ddc29d731c24ec0eed7d3d
Viktor Tochonov <viktochonov@gmail.com>
2023-12-07 14:19:05 +0200

    # Task 4. BLL Infrastructure classes was added. Custom classes ValidationException and MyAttributeNonEmptyList was created.

---
+++ Music Portal.BLL/Infrastructure/MyAttributeNonEmptyList.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music_Portal.BLL.Infrastructure
+{
+    // Перевіряє колекцію List, що вона не порожня
+    public class MyAttributeNonEmptyList : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {   
+                List<string> listToCheck = value as List<string>;
+                if (listToCheck.Count > 0) { return true; }
+            }
+            
+            return false;
+        }
+    }
+}
