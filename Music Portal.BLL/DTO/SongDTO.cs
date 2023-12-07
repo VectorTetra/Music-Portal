7605a4028aecea71cd80e83e7641b5e6319e5484
Viktor Tochonov <viktochonov@gmail.com>
2023-12-07 14:50:06 +0200

    # Task 6. BLL DTO was added and modified

--- Music Portal.BLL/DTO/SongDTO.cs
+++ Music Portal.BLL/DTO/SongDTO.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Music_Portal.BLL.Infrastructure;
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,14 @@ namespace Music_Portal.BLL.DTO
         [Required(ErrorMessage = "Поле обов'язкове для заповнення.")]
         [Display(Name = "Назва пісні")]
         public string Name { get; set; }
+        [MyAttributeNonEmptyList(ErrorMessage = "У пісні має бути хоча б один виконавець")]
+        [Display(Name = "Виконавці")]
+        public List<string> Singers { get; set; }
         [Required(ErrorMessage = "Поле обов'язкове для заповнення.")]
         [Display(Name = "Аудіофайл")]
         public string Path { get; set; }
         [Required(ErrorMessage = "Поле обов'язкове для заповнення.")]
         [Display(Name = "Жанр пісні")]
         public int GenreId { get; set; }
-        public string UploaderUserLogin { get; set; }
     }
 }
---
+++ Music Portal.BLL/DTO/UserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Portal.BLL.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле обов'язкове для заповнення.")]
+        [Display(Name = "Логін")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Поле обов'язкове для заповнення.")]
+        [Display(Name = "Ім'я")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Поле обов'язкове для заповнення.")]
+        [Display(Name = "Прізвище")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Поле обов'язкове для заповнення.")]
+        public int RoleId { get; set; }
+    }
+}
