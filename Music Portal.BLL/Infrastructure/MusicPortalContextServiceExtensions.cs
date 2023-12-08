39064371c2ee673ea12f7e9138565479217751b3
Viktor Tochonov <viktochonov@gmail.com>
2023-12-08 18:49:43 +0200

    # Task 8. Service Extensions for UnitOfWork and MusicPortalContext was added.

---
+++ Music Portal.BLL/Infrastructure/MusicPortalContextServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Music_Portal.DAL.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music_Portal.BLL.Infrastructure
+{
+    public static class MusicPortalContextServiceExtensions
+    {
+        public static void AddMusicPortalContext(this IServiceCollection services, string connection)
+        {
+            services.AddDbContext<MusicPortalContext>(options => options.UseSqlServer(connection));
+        }
+    }
+}
---
+++ Music Portal.BLL/Infrastructure/UnitOfWorkServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Music_Portal.DAL.Interfaces;
+using Music_Portal.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Portal.BLL.Infrastructure
+{
+    public static class UnitOfWorkServiceExtensions
+    {
+        public static void AddUnitOfWorkService(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, EFUnitOfWork>();
+        }
+    }
+}
